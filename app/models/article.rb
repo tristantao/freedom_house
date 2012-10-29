2640d1864bf0a0aad421b97378f0e86d36d7aa6d
tristan tao <tao.tristan@gmail.com>
2012-10-28 21:53:35 -0700

    article model validation fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ActiveRecord::Base
   belongs_to :source
   
   attr_accessible :title, :date, :text, :location, :author, :link
-
+  validate :title, :date, :text, :link, :presence => true
 end
