39fb6e1b7f78dd3641c36afa47d31733dc319650
Ferix9288 <felix.li@berkeley.edu>
2012-11-24 03:39:24 -0800

    added edit article functionality within list of articles

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,26 +1,43 @@
 class Admin::ArticlesController < ApplicationController
 
-	def index
-  	@articles = Article.all
-	end
+  def index
+    @articles = Article.all
+  end
 	
-	 def new
+  def new
     article = params[:article]
     if article
      s = Article.create(:title => article[:title], :location => article[:location], :date => article[:date], :author=> article[:author], :link => article[:link], :text => article[:text])
       if s.save
-        flash[:notice] = "Article #{article[:title]} has been created!"
-        redirect_to admin_articles_path
+	flash[:notice] = "Article #{article[:title]} has been created!"
+	redirect_to admin_articles_path
       else
-        flash[:warning] = "Error in creating article. Please try again."
+	flash[:warning] = "Error in creating article. Please try again."
       end
     end
   end
 	
-	def edit
-	end
+  def edit
+    @article = Article.find(params[:id]) 	  
+  end
 	
-	def delete
+  def update
+    @article = Article.find(params[:id])
+    @article.title = params[:article][:title]
+    @article.date = params[:article][:date]
+    @article.author = params[:article][:author]
+    @article.location = params[:article][:location]
+    @article.link = params[:article][:link]
+    @article.text = params[:article][:text]
+    if @article.save
+      flash[:notice] = "Successfully updated article!"
+    else
+      flash[:warning] = @article.errors.full_messages.join(". ")
+    end
+    redirect_to admin_article_action_path(:edit, @artcle.id)
+  end
+  
+  def delete
     @article = Article.find_by_id(params[:id])
     title = @article.title
     @article.delete
