af86f17f083f2d3d9c3f0b8c8afa4f35c778c37e
Christopher Balcells <Chris.Balcells10@gmail.com>
2012-12-05 05:18:08 -0800

    Added rspec tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have admin_privileges if user is admin' do
+    user = User.new
+    user.stub(:admin).and_return("true")
+    user.admin_privileges.should == "Yes"
+  end
+  it 'should not have admin_privileges if user is not admin' do
+    user = User.new
+    user.stub(:admin).and_return("false")
+    user.admin_privileges.should == "No"
+  end
+  it 'has a full name with a first name and last name' do
+    user = User.new
+    user.stub(:first_name).and_return("freddy")
+    user.stub(:last_name).and_return("so franky")
+    user.name.should == "freddy so franky"
+  end
 end
