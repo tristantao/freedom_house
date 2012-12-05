614e7a91c8ff17d76701ff01c5e8567d97af234a
Jose <jcarrillo7@live.com>
2012-12-04 16:57:15 -0800

    i dont like heroku

--- config/initializers/hirefire.rb
+++
@@ -1,8 +0,0 @@
-HireFire.configure do |config|
-  if Rails.env.production?
-    config.environment = :heroku
-   else
-    config.environment = :local
-  end
-  # add any other configuration you want here
-end
